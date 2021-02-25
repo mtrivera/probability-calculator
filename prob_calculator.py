cca6d9a426a05d1d0940410a33ca31bf94c4bd5a
Miguel T Rivera <mrivera108@outlook.com>
2021-02-24 18:41:41 -0800

    Code Hat class

--- prob_calculator.py
+++ prob_calculator.py
@@ -3,7 +3,29 @@ import random
 # Consider using the modules imported above.
 
 class Hat:
-    pass
+    def __init__(self, **balls):
+        self.contents = self.create_balls_list(balls)
+
+    def get_contents(self):
+        return self.contents
+
+    def create_balls_list(self, dict):
+        result = []
+        for ball_type, ball_count in dict.items():
+            for _ in range(ball_count):
+                result.append(ball_type)
+        return result
+
+    def draw(self, num_of_balls):
+        if num_of_balls > len(self.get_contents()):
+            self.get_contents()
+
+        drawn_balls = []
+        for _ in range(num_of_balls):
+            random_index = random.randint(0, len(self.get_contents()) - 1)
+            drawn_balls.append(self.contents.pop(random_index))
+        return drawn_balls
+
 
 def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
     pass
